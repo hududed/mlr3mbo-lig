db2737095cc95205b5fc6f8e762a268979d46f9f
hududed <hudwahab@gmail.com>
2024-04-20 14:55:48 -0600

    adding propose component 1st batch

---
+++ main.R
@@ -0,0 +1,121 @@
+# %%
+library(mlr3mbo)
+library(mlr3)
+library(mlr3learners)
+library(bbotk)
+library(data.table)
+library(tibble)
+
+round_to_nearest <- function(x, value) {
+  to_nearest = value
+  if (is.data.table(x) || is.data.frame(x)) {
+    x = lapply(x, function(col) {
+      if (is.numeric(col)) {
+        return(round(col / to_nearest) * to_nearest)
+      } else {
+        return(col)
+      }
+    })
+    x = setDT(x) # Convert the list to a data.table
+  } else if (is.numeric(x)) {
+    x = round(x / to_nearest) * to_nearest
+  }
+  return(x)
+}
+# Load data
+file <- "LIG_data.csv"
+data <- as.data.table(read.csv(file))
+
+# Set up the search space
+domain = ps(Power = p_int(lower = 10, upper = 70),
+            Speed = p_int(lower = 10, upper = 60),
+            DPI = p_int(lower = 1, upper = 7),
+            N2gas = p_int(lower = 0, upper = 1),
+            Defocus = p_dbl(lower = -0.3, upper = 0.3))
+codomain = ps(Resistance = p_dbl(tags = "minimize"))
+
+# Set up the archive, surrogate, acquisition function, and acquisition optimizer
+archive = Archive$new(search_space = domain, codomain = codomain)
+archive$add_evals(xdt = data[, c("Power", "Speed", "DPI", "N2gas", "Defocus")],
+                  ydt = data[, c("Resistance")])
+
+
+surrogate <- srlrn(default_rf(), archive = archive)
+acq_function = acqf("ei", surrogate = surrogate)
+acq_optimizer = acqo(
+  opt("random_search", batch_size = 1000),
+  terminator = trm("evals", n_evals = 1000),
+  acq_function = acq_function)
+set.seed(42)
+
+q = 5  # we want 5 proposals
+lie = data.table()  # needed for constant liear
+liar = mean  # liar function, e.g., constant mean
+
+# Obtain the first candidate
+acq_function$surrogate$update()
+acq_function$update()
+candidate = acq_optimizer$optimize()
+candidate <- round_to_nearest(candidate, 0.01)
+
+# prepare lie objects
+tmp_archive = archive$clone(deep = TRUE)
+acq_function$surrogate$archive = tmp_archive
+lie[, archive$cols_y := liar(archive$data[[archive$cols_y]])]
+candidate_new = candidate
+
+# obtain the other q-1 candidates using fake archive
+for (i in seq_len(q)) {
+  prediction = acq_function$surrogate$predict(candidate_new)
+  col_names <- c(paste0(archive$cols_y[1], "_mean"), paste0(archive$cols_y[1], "_se"))
+  # Add new columns to candidate
+  for (col_name in col_names) {
+    if (!col_name %in% names(candidate)) {
+      candidate[, (col_name) := NA]
+    }
+  }
+  # Get the predicted mean and standard error
+  prediction <- acq_function$surrogate$predict(candidate_new)
+  candidate_new[, (col_names) := .(prediction$mean[1], prediction$se[1])]
+
+  if (i > 1) {
+    candidate <- rbind(candidate, candidate_new, fill = TRUE)
+  }
+  if (i <= q) {
+    tmp_archive$add_evals(xdt = candidate_new,
+                          xss_trafoed = transform_xdt_to_xss(candidate_new, tmp_archive$search_space),
+                          ydt = lie)
+  # update all objects with lie and obtain new candidate
+    acq_function$surrogate$update()
+    acq_function$update()
+    candidate_new = acq_optimizer$optimize()
+    candidate_new <- round_to_nearest(candidate_new, 0.01)
+  }
+}
+
+x2_dt <- as.data.table(candidate)
+print(x2_dt)
+# Prepare data_with_preds
+candidate_with_preds <- candidate[, -c(".already_evaluated","x_domain"), with = FALSE]
+tmp_data_with_preds <- rbindlist(list(data, candidate_with_preds), fill = TRUE)
+# Select only the last rows of length x2_dt
+x2_with_preds <- tmp_data_with_preds[(nrow(tmp_data_with_preds) -
+                                      nrow(x2_dt)+1):nrow(tmp_data_with_preds), ]
+data_with_preds <- rbindlist(list(data, x2_with_preds), fill = TRUE)
+
+data_no_preds <- subset(data_with_preds,
+                        select = -c(acq_ei, Resistance_mean, Resistance_se))
+# reset the working archive to the actual one and not the temporary lie archive
+acq_function$surrogate$archive = archive
+
+
+# Save the data tables as CSV files
+dir_path <- "."
+data.table::fwrite(data_no_preds, paste0(dir_path, "/output.csv"))
+data.table::fwrite(data_with_preds, paste0(dir_path, "/output_with_preds.csv"))
+
+# Save RDS files
+saveRDS(archive,  paste0(dir_path,  "/archive.rds"))
+saveRDS(acq_function, paste0(dir_path, "/acq_function.rds"))
+saveRDS(acq_optimizer, paste0(dir_path, "/acq_optimizer.rds"))
+# %%
